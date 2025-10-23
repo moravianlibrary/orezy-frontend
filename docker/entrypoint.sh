f220d423c882124961e02f19f21478ef5af0476b
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2025-10-23 07:06:21 +0200

    ci: add main workflow

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,8 @@ cat <<EOF > /usr/share/nginx/html/assets/env.json
   "devMode": ${APP_DEV_MODE:-false},
   "environmentName": "${APP_ENV_NAME:-docker runtime}",
   "environmentCode": "${APP_ENV_CODE:-docker}",
-  
-  "serverBaseUrl": "${APP_DATA_SERVER_URL}"  
+
+  "serverBaseUrl": "${APP_DATA_SERVER_URL}"
 }
 EOF
 
